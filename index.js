72d539583a7a5715350b66e3c2ce8765ca4312fb
Anastasia Mironova <anast.mir22@yandex.ru>
2020-12-28 01:33:44 +0300

    add send mail

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var $menu = $(".header");
+  let $menu = $(".header");
 
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100 && $menu.hasClass("default")) {
@@ -9,3 +9,35 @@ $(document).ready(function() {
     }
   }); //scroll
 });
+
+$(".preview__cat-logo").mousemove(function(e) {
+  let x = e.pageX + 12;
+});
+
+document.onmousemove = function(event) {
+  let x = event.x - 50;
+  let y = event.y - 50;
+  document.querySelector(".left-eye").style.transform = `rotate(${57.2958 *
+    arcctg(x, y)}deg)`;
+  document.querySelector(".right-eye").style.transform = `rotate(${57.2958 *
+    arcctg(x - 19, y)}deg)`;
+  document.querySelector(
+    ".feedback__left-eye"
+  ).style.transform = `rotate(${57.2958 * arcctg(x, y)}deg)`;
+  document.querySelector(
+    ".feedback__right-eye"
+  ).style.transform = `rotate(${57.2958 * arcctg(x - 19, y)}deg)`;
+  document.querySelector(
+    ".contact__left-eye"
+  ).style.transform = `rotate(${57.2958 * arcctg(-x, y)}deg)`;
+  document.querySelector(
+    ".contact__right-eye"
+  ).style.transform = `rotate(${57.2958 * arcctg(-x - 19, y)}deg)`;
+
+  function arcctg(x, y) {
+    if (x > 0 && y > 0) return Math.PI / 2 - Math.atan(x / y);
+    if (x < 0 && y > 0) return Math.PI / 2 - Math.atan(x / y);
+    if (x < 0 && y < 0) return Math.PI + Math.atan(y / x);
+    if (x > 0 && y < 0) return (3 * Math.PI) / 2 + Math.abs(Math.atan(x / y));
+  }
+};
--- send.php
+++ send.php
@@ -1,44 +1,54 @@
-if (!empty($_POST['name'])) {
-print("Имя: " . $_POST['name']);
-print("<br>Email: " . $_POST['email']);
+<?php
+
+if (isset($_POST['name'])) {
+    $name = $_POST['name'];
 }
-
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$project_description = $_POST['project-description'];
-$file = $_POST['file'];
-
-
-$name = htmlspecialchars($name);
-$phone = htmlspecialchars($phone);
-$email = htmlspecialchars($email);
-$project_description = htmlspecialchars($project_description);
-$file = htmlspecialchars($file);
-
-$name = urldecode($name);
-$phone = urldecode($phone);
-$email = urldecode($email);
-$project_description = urldecode($project_description);
-$file = urldecode($file);
-
-$name = trim($name);
-$phone = trim($phone);
-$email = trim($email);
-$project_description = trim($project_description);
-
-echo $name
-echo <br>
-echo $phone
-echo <br>
-echo $email
-echo <br>
-echo $project_description
-echo <br>
-
-if(mail("anast.mir22@yandex.ru", "Заявка с сайта GUZANOV STUDIO", "Имя:".$name. "\r\n Телефон".$phone.". E-mail: ".$email "".$project_description. "Файл".$file.,"From: example2@mail.ru \r\n"))
-{
-echo "сообщение успешно отправлено";
-} else {
-echo "при отправке сообщения возникли ошибки";
+if (isset($_POST['phone'])) {
+    $phone = $_POST['phone'];
 }
+if (isset($_POST['email'])) {
+    $email = $_POST['email'];
+}
+if (isset($_POST['project-description'])) {
+    $message = $_POST['project-description'];
+}
+$myaddres  = "anast.mir22@yandex.ru"; // кому отправляем
+
+
+$mes = "Тема: Заявка с сайта Guzanov Studio \nИмя: $name \n Email: $email \n Телефон: $phone \n Описание проекта: $message";
+
+$sub = 'Заявка'; //сабж
+$emailTitle = 'Заявка с сайта Guzanov Studio'; // от кого
+
+$send = mail($myaddres, $sub, $mes, "Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+// отправляем письмо 
+
+ini_set('short_open_tag', 'On');
+header('Refresh: 3; URL=index.html');
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="refresh" content="3; url=index.html">
+    <title>Спасибо! Мы свяжемся с вами!</title>
+    <meta name="generator">
+    <script type="text/javascript">
+        setTimeout('location.replace("/index.html")', 3000);
+    </script>
+    <style>
+        body {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Спасибо! Мы свяжемся с вами!</h1>
+</body>
+
+</html>
